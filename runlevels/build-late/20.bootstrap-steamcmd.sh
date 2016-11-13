a9d8ccca816c3462a2f916e9365ae34348dc5e21
W-Mark Kubacki <wmark@hurrikane.de>
2016-11-13 00:35:50 +0000

    dgr: catch the exact version of steamcmd

--- runlevels/build-late/20.bootstrap-steamcmd.sh
+++ runlevels/build-late/20.bootstrap-steamcmd.sh
@@ -10,3 +10,12 @@ chpst -u steam -- \
 
 # Allows steamcmd to update itself as unprivileged user.
 chown -R steam:steam /opt/steam
+
+# Gets the client's version so we can identify it for Valve to properly report errors.
+version=$(</opt/steam/package/steam_cmd_linux.manifest grep -m 1 -A 3 -F '"linux"' | grep -F version | awk '{print $2}')
+echo_purple "This is steamcmd version: ${version}"
+cat >/dgr/builder/attributes/steamcmd.yml <<EOF
+default:
+  steamcmd:
+    version: ${version}
+EOF
